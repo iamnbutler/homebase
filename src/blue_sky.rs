4a3db5701aac03d56751ce11b07d98183e78e8c5
Nate Butler <iamnbutler@gmail.com>
2024-12-14 21:45:45 -0500

    Checkpoint

--- src/blue_sky.rs
+++ src/blue_sky.rs
@@ -1,28 +1,53 @@
 use std::env;
 
-use atrium_api::client::AtpServiceClient;
+use atrium_api::{
+    agent::{store::MemorySessionStore, AtpAgent},
+    app::bsky::feed::{defs::FeedViewPost, get_author_feed},
+    types::{
+        string::{AtIdentifier, Handle},
+        LimitedNonZeroU8, Object,
+    },
+};
 use atrium_xrpc_client::reqwest::ReqwestClient;
 
 /// Initialize the Blue Sky client and create a session.
-pub async fn init() -> Result<(), Box<dyn std::error::Error>> {
+pub async fn init() -> Result<BlueSkyClient, Box<dyn std::error::Error>> {
     let blue_sky_username = env::var("BLUE_SKY_USERNAME").expect("BLUE_SKY_USERNAME not set");
     let blue_sky_password = env::var("BLUE_SKY_PASSWORD").expect("BLUE_SKY_PASSWORD not set");
 
-    let client = AtpServiceClient::new(ReqwestClient::new("https://bsky.social"));
-    let result = client
-        .service
-        .com
-        .atproto
-        .server
-        .create_session(
-            atrium_api::com::atproto::server::create_session::InputData {
-                auth_factor_token: None,
-                identifier: blue_sky_username.into(),
-                password: blue_sky_password.into(),
-            }
-            .into(),
-        )
-        .await;
+    let agent = AtpAgent::new(
+        ReqwestClient::new("https://bsky.social"),
+        MemorySessionStore::default(),
+    );
+    agent.login(&blue_sky_username, &blue_sky_password).await?;
+    let result = agent.api.com.atproto.server.get_session().await?;
     println!("{:?}", result);
-    Ok(())
+    Ok(BlueSkyClient { agent })
+}
+
+pub struct BlueSkyClient {
+    agent: AtpAgent<MemorySessionStore, ReqwestClient>,
+}
+
+impl BlueSkyClient {
+    pub async fn get_posts(
+        &self,
+        handle: &str,
+        limit: u8,
+    ) -> Result<Vec<FeedViewPost>, Box<dyn std::error::Error>> {
+        let actor = Handle::new(handle.to_string())?;
+        let paramaters_data = get_author_feed::ParametersData {
+            actor: actor.into(),
+            limit: Some(LimitedNonZeroU8::<100>::try_from(limit)?),
+            cursor: None,
+            filter: None,
+            include_pins: None,
+        };
+
+        let params: Object<get_author_feed::ParametersData> = paramaters_data.into();
+
+        let response = self.agent.api.app.bsky.feed.get_author_feed(params).await?;
+
+        Ok(response.data.feed)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 mod blue_sky;
 
+use blue_sky::BlueSkyClient;
 use dotenv::dotenv;
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
 use reqwest::get;
+
+pub struct AppContext {
+    blue_sky_client: Arc<BlueSkyClient>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
-    blue_sky::init().await?;
+    let blue_sky_client = Arc::new(blue_sky::init().await?);
+
+    let app_context = AppContext { blue_sky_client };
+
+    let posts = app_context
+        .blue_sky_client
+        .get_posts("nate.rip", 10)
+        .await?;
+    for post in posts {
+        println!("{:?}: {:?}", post.post.author.handle, post.post.record);
+    }
 
     let resp = get("https://httpbin.org/ip")
         .await?
